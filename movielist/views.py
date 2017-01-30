146445f09809a1db8d546cb5ec8b3397ef986737
Daan Smits <Daan Smits>
2017-01-30 13:14:14 -0500

    Project initialization, before changing to postgresql

--- movielist/views.py
+++ movielist/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the homepage.")
+    return HttpResponse("Hello, world. You're at the homepage. Soon, you can keep track of which movies you've already seen and score them")
