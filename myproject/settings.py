60274e384de1383a0dd3fc5ccb710cb96f276cb0
Daan Smits <Daan Smits>
2017-02-04 22:53:57 -0500

    Updated database settings for Heroku

--- myproject/settings.py
+++ myproject/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'd9mgi09jubttvq',
         'USER': 'gugaqtskzwhpuq',
-        'PASSWORD': '242c81e177bf747fe1159cb0da4edcfc6fd205e5341a6a911629f17089749bb0'
+        'PASSWORD': '242c81e177bf747fe1159cb0da4edcfc6fd205e5341a6a911629f17089749bb0',
         'HOST': 'ec2-75-101-142-182.compute-1.amazonaws.com',
         'PORT': '5432',
     }
