c1f66770794b09715375678be054f0176d373810
Daan Smits <Daan Smits>
2017-03-04 17:18:35 -0500

    deleted all user related items before tutorial Chris Hawkes

--- myproject/urls.py
+++ myproject/urls.py
@@ -21,5 +21,4 @@ urlpatterns = [
 	url(r'^$', views.home),
 	url(r'^movielist/', include('movielist.urls', namespace="movielist")),
     url(r'^admin/', admin.site.urls),
-    url(r'^accounts/', include('django.contrib.auth.urls'))
 ]
